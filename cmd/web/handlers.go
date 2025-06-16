70669e93beb2e832413e04d657680fc25213a3b1
Viggie1 <haxshapova@gmail.com>
2025-06-15 22:25:19 -0400

    Implements html templates and renders them with one of the routes.

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,12 +3,33 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"html/template"
+	"log"
 	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Server", "Go")
-	w.Write([]byte("Hello from SnippetBox\n"))
+
+	files := []string{
+		"./ui/html/base.tmpl",
+		"./ui/html/pages/home.tmpl",
+	}
+
+	//we use the ... to pass the contents
+	ts, err := template.ParseFiles(files...)
+
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	err = ts.ExecuteTemplate(w, "base", nil)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
 }
 
 func snippetView(w http.ResponseWriter, r *http.Request) {
